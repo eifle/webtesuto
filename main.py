3687ed4376cac7a228201274de2b8f0a187e2c2b
eifle <100400840+eifle@users.noreply.github.com>
2022-03-11 14:38:39 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,37 @@
+import requests as rqs
+from bs4 import BeautifulSoup as bsp
+
+blocks = {  # texts // str
+    'title': '',
+    'content': '', #0
+    'input': '', #1
+    'output': '', #2
+    'sample_input': '', #3
+    'sample_output': '', #4
+    'hint': '',
+    'tag': '',
+    'information': '',
+    'source': ''
+}
+
+def main():
+    pid = 'a001'
+    url = 'http://203.64.191.163/ShowProblem?problemid={}'
+    response = rqs.get(url.format(pid))
+    html = bsp(response.text, 'html.parser')
+    content = html.find_all(name = 'div', attrs = {'class', 'problembox'})
+    
+    print(content[4])
+    
+    
+    
+if __name__ == '__main__':
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
